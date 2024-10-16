a4fa50a02e177eb20024f03fcac18528d4f139d0
HS-hyeokjin <youngurwls@gmail.com>
2024-10-16 19:59:24 +0900

    feat : config 추가

---
+++ backend/ssharp/src/main/java/com/tls/ssharp/config/AuditingConfig.java
@@ -0,0 +1,10 @@
+package com.tls.ssharp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class AuditingConfig {
+}
+
---
+++ backend/ssharp/src/main/java/com/tls/ssharp/config/RedisConfig.java
@@ -0,0 +1,28 @@
+package com.tls.ssharp.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.*;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.*;
+
+@Configuration
+public class RedisConfig {
+
+  @Value("${spring.data.redis.host}")
+  private String redisHost;
+
+  @Value("${spring.data.redis.port}")
+  private int redisPort;
+
+  @Bean
+  public RedisConnectionFactory redisConnectionFactory() {
+    return new LettuceConnectionFactory(redisHost, redisPort);
+  }
+  @Bean
+  public RedisTemplate<?, ?> redisTemplate() {
+    RedisTemplate<?, ?> redisTemplate = new RedisTemplate<>();
+    redisTemplate.setConnectionFactory(redisConnectionFactory());
+    return redisTemplate;
+  }
+}
