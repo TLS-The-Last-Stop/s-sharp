4df476b4a237ab804099e724a84c63e85c535412
HS-hyeokjin <youngurwls@gmail.com>
2024-10-16 14:05:23 +0900

    init : 프로젝트 초기화

---
+++ backend/ssharp/src/main/java/com/tls/ssharp/SsharpApplication.java
@@ -0,0 +1,13 @@
+package com.tls.ssharp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsharpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsharpApplication.class, args);
+	}
+
+}
---
+++ backend/ssharp/src/test/java/com/tls/ssharp/SsharpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tls.ssharp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsharpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
