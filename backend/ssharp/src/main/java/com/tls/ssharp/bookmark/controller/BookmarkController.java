a74f587c4508f9e3971f4e01891d77ba00fba56d
kde0707 <gammaeom1@gmail.com>
2024-10-17 09:41:34 +0900

    feat: 북마크 기능

--- backend/ssharp/src/main/java/com/tls/ssharp/bookmark/controller/BookmarkController.java
+++ backend/ssharp/src/main/java/com/tls/ssharp/bookmark/controller/BookmarkController.java
@@ -1,5 +1,6 @@
 package com.tls.ssharp.bookmark.controller;
 
+import com.tls.ssharp.auth.common.dto.CommonApiResponse;
 import com.tls.ssharp.bookmark.dto.request.BookmarkRequest;
 import com.tls.ssharp.bookmark.entity.Bookmark;
 import com.tls.ssharp.bookmark.service.BookmarkService;
@@ -7,17 +8,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/bookmarks")
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class BookmarkController {
 
   private final BookmarkService bookmarkService;
 
-  @PostMapping
-  public void markBookmark(@RequestBody BookmarkRequest bookmarkRequest) {
+  @PostMapping("/bookmark")
+  public CommonApiResponse markBookmark(@RequestBody BookmarkRequest bookmarkRequest) {
     System.out.println("bookmarkRequest.getUserId() = " + bookmarkRequest.getUserId());
     System.out.println("bookmarkRequest.getPostId() = " + bookmarkRequest.getPostId());
     bookmarkService.saveBookmark(bookmarkRequest);
+    return CommonApiResponse.createNoContent("성공함");
   }
 
 }
