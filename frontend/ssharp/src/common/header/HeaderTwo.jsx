ffa10234bb1eece663179e8b9cda7b9ce191752d
kde0707 <gammaeom1@gmail.com>
2024-10-20 14:56:20 +0900

    feat: 로그아웃 구현

--- frontend/ssharp/src/common/header/HeaderTwo.jsx
+++ frontend/ssharp/src/common/header/HeaderTwo.jsx
@@ -1,51 +1,46 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import Nav from './Nav';
+import Cookies from 'js-cookie';
 
 const HeaderTwo = () => {
-  if (window.location.pathname === '../pages/write.jsp') return null;
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  const openMenu = () => {
-    setIsMenuOpen(true);
-  };
-  const closeMenu = () => {
-    setIsMenuOpen(false);
-  };
-
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [accessToken, setAccessToken] = useState(Cookies.get('accessToken'));
+  const navigate = useNavigate();
+
+  const openMenu = () => setIsMenuOpen(true);
+  const closeMenu = () => setIsMenuOpen(false);
 
   useEffect(() => {
-    const handleResize = () => {
-      setWindowWidth(window.innerWidth);
-    };
-
+    const handleResize = () => setWindowWidth(window.innerWidth);
     window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
+  const handleLogout = () => {
+    Cookies.remove('refreshToken');
+    Cookies.remove('accessToken');
+    setAccessToken(null);
+    navigate('/');
+  };
+
+  useEffect(() => {
+    const token = Cookies.get('accessToken');
+    setAccessToken(token);
   }, []);
 
   return (
-    <>
       <header className='header-style-1'>
-        <div
-          className={`header-navbar navbar-sticky ${
-            windowWidth <= 991 ? 'mobile-menu' : ''
-          }`}
-        >
+        <div className={`header-navbar navbar-sticky ${windowWidth <= 991 ? 'mobile-menu' : ''}`}>
           <div className='container'>
             <div className='d-flex align-items-center justify-content-between'>
               <div className='site-logo'>
                 <Link to='/'>
                   <img
-                    src={`${
-                      import.meta.env.VITE_API_URL
-                    }/assets/images/logo.png`}
-                    alt='Course Thumb'
-                    className='img-fluid'
+                      src={`${import.meta.env.VITE_API_URL}/assets/images/logo.png`}
+                      alt='Course Thumb'
+                      className='img-fluid'
                   />
                 </Link>
               </div>
@@ -56,34 +51,35 @@ const HeaderTwo = () => {
                 </a>
               </div>
 
-              <nav
-                className={`site-navbar ms-auto ${isMenuOpen ? 'menu-on' : ''}`}
-              >
+              <nav className={`site-navbar ms-auto ${isMenuOpen ? 'menu-on' : ''}`}>
                 <Nav />
-
                 {isMenuOpen && (
-                  <a href='#' onClick={closeMenu} className='nav-close'>
-                    <i className='fal fa-times'></i>
-                  </a>
+                    <a href='#' onClick={closeMenu} className='nav-close'>
+                      <i className='fal fa-times'></i>
+                    </a>
                 )}
               </nav>
 
               <div className='header-btn d-none d-xl-block'>
-                <Link to='/login' className='login'>
-                  Login
-                </Link>
-                <Link
-                  to='/register'
-                  className='btn btn-main-2 btn-sm-2 rounded'
-                >
-                  Sign up
-                </Link>
+                {accessToken ? (
+                    <button onClick={handleLogout} className='btn btn-main-2 btn-sm-2 rounded' type='button'>
+                      로그아웃
+                    </button>
+                ) : (
+                    <>
+                      <Link to='/login' className='login'>
+                        로그인
+                      </Link>
+                      <Link to='/register' className='btn btn-main-2 btn-sm-2 rounded'>
+                        회원가입
+                      </Link>
+                    </>
+                )}
               </div>
             </div>
           </div>
         </div>
       </header>
-    </>
   );
 };
 
